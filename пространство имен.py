c11f1c2b067015f8bb3b8526ea48a80d958e1a0a
anask05 <168666362+anask05@users.noreply.github.com>
2024-07-03 16:51:16 +0300

    Add files via upload

---
+++ пространство имен.py
@@ -0,0 +1,14 @@
+def test_function():
+    print("test_function")
+    def inner_function():
+        print("Я в области видимости функции test_function")
+        return
+    inner_function()
+    return
+
+
+test_function()
+
+
+
+
